f29867abb7bcc439f072c9c13ee9ba814bcbe7e3
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-06 04:29:11 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  counter = 0
+  newArray = []
+  while array[counter] do
+    temp = (array[counter])**2
+    newArray.push(temp)
+    counter += 1
+  end
+  return newArray
 end
+
